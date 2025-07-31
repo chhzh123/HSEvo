65e895bd92d07d2e05f7ed73e29a46cfdc69c688
hins-hu <zh223@cornell.edu>
2025-07-30 23:51:03 -0700

    Add pdptw for reevo

--- baselines/reevo/reevo.py
+++ baselines/reevo/reevo.py
@@ -105,12 +105,13 @@ class ReEvo:
             "code_path": f"problem_iter{self.iteration}_code0.py",
             "code": code,
             "response_id": 0,
+            "timeout": False,
         }
         self.seed_ind = seed_ind
         self.population = self.evaluate_population([seed_ind])
 
         # If seed function is invalid, stop
-        if not self.seed_ind["exec_success"]:
+        if not self.seed_ind["exec_success"] and not self.seed_ind["timeout"]:
             raise RuntimeError(f"Seed function is invalid. Please check the stdout file in {os.getcwd()}.")
 
         self.update_iter()
@@ -246,6 +247,7 @@ class ReEvo:
                     logging.info(f"Error for response_id {response_id}: {e}")
                     population[response_id] = self.mark_invalid_individual(population[response_id], str(e))
                     inner_run.kill()
+                    self.seed_ind["timeout"] = True
                     continue
 
                 individual = population[response_id]
